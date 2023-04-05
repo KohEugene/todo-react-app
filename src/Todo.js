cf50d4a0188955d75170bb1e45a0ffdeb0cf40cd
Koh Eugene <kyg030320@sookmyung.ac.kr>
2023-04-05 18:08:32 +0900

    Todo.js 여러 개의 오브젝트를 매개변수로 전달

--- src/Todo.js
+++ src/Todo.js
@@ -3,8 +3,9 @@ import React from 'react';
 class Todo extends React.Component { 
     constructor(props){
         super(props);
-        this.state ={ item: props.item };   // 매개변수 item 의 변수/값을 item에 대입 
+        this.state ={ item: props.item };   // 매개변수 item의 변수/값을 item에 대입 
     }
+
     render(){ 
         return (
             <div className="Todo"> 
@@ -14,10 +15,10 @@ class Todo extends React.Component {
                     name={this.state.item.id}
                     checked={this.state.item.done}  // item.done 값으로 렌더링하란 의미 
                 />
-                <label for={this.state.item.id}>{this.state.item.title}</label> 
-
+                <label for={this.state.item.id}>{this.state.item.title}</label>
             </div>
-        ); 
-    }
+        );
+    } 
 }
+
 export default Todo;
